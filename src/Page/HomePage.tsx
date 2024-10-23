cc3777918ce40c329e4572a2b6bb9582c71a223f
Nanor-Bright-Suka <147738677+Nanor-Bright-Suka@users.noreply.github.com>
2024-10-23 12:03:20 +0000

    Responsiveness laptop view (#3)
    * Adding delete and edit functionality
    
    * Done with the app for small mobile view
    
    * Responsiveness
    
    ---------
    
    Co-authored-by: Nanor-0196-Bright-415-Nanor <147738677+Nanor-0196-Bright-415-Nanor@users.noreply.github.com>

--- src/Page/HomePage.tsx
+++ src/Page/HomePage.tsx
@@ -48,44 +48,54 @@ const HomePage: React.FC = () => {
     };
 
       return (
-        <div >
+        <div className="max-lg:max-w-xl max-lg:mx-auto  lg:max-w-xl lg:mx-auto max-lg:mt-24">
             <div className="flex justify-end">
                     <> 
                    <Link to={"/"}>  
-                        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" className="max-sm:w-[65px] max-sm:h-[100px]" ><path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5} d="M19.523 21.99H4.488c-1.503 0-2.663-1.134-2.466-2.624l.114-.869c.207-1.2 1.305-1.955 2.497-2.214L11.928 15h.144l7.295 1.283c1.212.28 2.29.993 2.497 2.214l.114.88c.197 1.49-.963 2.623-2.466 2.623zM17 7A5 5 0 1 1 7 7a5 5 0 0 1 10 0" /></svg>
+                          <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" className="max-sm:w-[65px] max-sm:h-[100px] max-md:w-[50%] max-md:h-[100px]
+                          max-lg:w-[50%] max-lg:h-[90px] lg:w-[50%] lg:h-[90px]" >
+                              <path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5} d="M19.523 21.99H4.488c-1.503 0-2.663-1.134-2.466-2.624l.114-.869c.207-1.2 1.305-1.955 2.497-2.214L11.928 15h.144l7.295 1.283c1.212.28 2.29.993 2.497 2.214l.114.88c.197 1.49-.963 2.623-2.466 2.623zM17 7A5 5 0 1 1 7 7a5 5 0 0 1 10 0" />
+                          </svg>
                         </Link>
                          </>
                 
             </div>
 
             <div className="bg-slate-300 p-5 rounded-lg mb-6">
-                <h3 className="max-sm:text-4xl text-center my-5">Hello, {name}</h3>
-                <p className="text-center max-sm:text-sm">{date}</p>
-                <p className="text-right max-sm:text-sm">{time}</p>
+                <h3 className="max-sm:text-4xl text-center my-5 max-md:text-5xl max-lg:text-4xl lg:text-4xl">Hello, {name}</h3>
+                <p className="text-center max-sm:text-sm max-md:text-2xl max-lg:text-2xl lg:text-2xl">{date}</p>
+                <p className="text-right max-sm:text-sm max-md:text-xl max-lg:text-xl lg:text-xl">{time}</p>
             </div>
-            
-            <div className="flex justify-center items-center border-[1px] border-neutral-300 mb-7">
-                <InputField placeholder="Search Item" className="max-sm:w-[100%]" setFilteredTasks={setFilteredTasks} />
-                <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" className="max-sm:w-[60px] max-sm:h-[65px] bg-[#5EC9E7]"><path fill="white" d="m19.6 21l-6.3-6.3q-.75.6-1.725.95T9.5 16q-2.725 0-4.612-1.888T3 9.5t1.888-4.612T9.5 3t4.613 1.888T16 9.5q0 1.1-.35 2.075T14.7 13.3l6.3 6.3zM9.5 14q1.875 0 3.188-1.312T14 9.5t-1.312-3.187T9.5 5T6.313 6.313T5 9.5t1.313 3.188T9.5 14" /></svg>
+           
+            <div className="flex justify-center items-center border-[1px] border-neutral-300 mt-20">
+                <InputField placeholder="Search Item" className="max-sm:w-[100%] max-md:w-[100%] max-md:h-[68px] max-md:text-3xl max-lg:w-[100%] max-lg:h-[68px] max-lg:text-2xl lg:w-[100%] lg:h-[68px] lg:text-2xl" setFilteredTasks={setFilteredTasks} />
+                  <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" className="max-sm:w-[63px] max-sm:h-[65px] bg-[#5EC9E7] max-md:w-[70px] max-md:h-[70px] max-lg:w-[70px] max-lg:h-[70px] lg:w-[70px] lg:h-[70px]">
+                      <path fill="white" d="m19.6 21l-6.3-6.3q-.75.6-1.725.95T9.5 16q-2.725 0-4.612-1.888T3 9.5t1.888-4.612T9.5 3t4.613 1.888T16 9.5q0 1.1-.35 2.075T14.7 13.3l6.3 6.3zM9.5 14q1.875 0 3.188-1.312T14 9.5t-1.312-3.187T9.5 5T6.313 6.313T5 9.5t1.313 3.188T9.5 14" />
+                  </svg>
             </div>
             {filteredTasks.length > 0 ? (
         filteredTasks.map((task, index) => (
           <div key={index} className="mt-5 bg-slate-300 p-2 rounded">
-                <h5>{task.name}</h5>
+
+                <h5 className="max-md:text-3xl max-lg:text-2xl lg:text-2xl">{task.name}</h5>
                 <div className="flex justify-between">  
-                <p>{task.time}</p>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 48 48" className="max-sm:w-[50px] max-sm:h-[50px]" onClick={() => deleteFilteredTask(index)}><path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" d="M31.945 7.624L28.84 4.5h-9.68l-3.105 3.124H8.872v4.647h30.256V7.624zm-19.9 4.647h23.95v28.124A3.106 3.106 0 0 1 32.89 43.5H15.15a3.106 3.106 0 0 1-3.105-3.105zM24 17.886v20m6-20v20m-12-20v20"></path></svg>
+                <p className="max-md:text-xl mt-4 max-lg:text-xl lg:text-xl">{task.time}</p>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 48 48" className="max-sm:w-[50px] max-sm:h-[50px] max-md:w-[50px] max-md:h-[50px] max-lg:w-[50px] max-lg:h-[50px] lg:w-[50px] lg:h-[50px]" onClick={() => deleteFilteredTask(index)}><path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" d="M31.945 7.624L28.84 4.5h-9.68l-3.105 3.124H8.872v4.647h30.256V7.624zm-19.9 4.647h23.95v28.124A3.106 3.106 0 0 1 32.89 43.5H15.15a3.106 3.106 0 0 1-3.105-3.105zM24 17.886v20m6-20v20m-12-20v20"></path></svg>
+
                     </div>
           </div>
         ))
       ) : (
         <p></p>
-      )}
-            <div className="flex justify-between items-center rounded-md">
-            <h4 className="text-xl">Task Completion Status:</h4>
+
+              )}
+             
+              <div className="flex justify-between items-center rounded-md max-md:mt-16 max-lg:mt-16 lg:mt-16">
+            <h4 className="text-xl max-md:text-3xl max-md:pl-10 max-lg:text-3xl max-lg:pl-10 lg:text-3xl lg:pl-10">Task Completion Status:</h4>
+
                   <TaskStatus completedTasksNo={completedTasksNo} totalTasksNo={totalTasksNo} />  
             </div>
-
+            <hr className="bg-slate-300 w-full border-2 m-5"/>
             <div>
                   <TaskItem handleCompletedTask={updateTaskCompletion} />
             </div>
--- src/Page/WelcomPage.tsx
+++ src/Page/WelcomPage.tsx
@@ -26,21 +26,21 @@ const WelcomPage: React.FC = () => {
   };
 
   return (
-      <div className='flex flex-col justify-center items-center gap-10'>
+    <div className='flex flex-col justify-center items-center gap-10 max-md:gap-16  max-lg:max-w-2xl max-lg:mx-auto max-lg:gap-10 max-lg:border-2 max-lg:mt-28 lg:max-w-lg lg:mx-auto lg:gap-10 lg:border-2'>
       
-          <h1 className='text-4xl'>Welcome !</h1>
-          <img src={Welcome_Logo} alt="Welcome Logo" className='max-sm:w-[20.04%] max-sm:h-[18.14%] rounded-md' />
+          <h1 className='max-lg:text-5xl text-4xl max-md:text-[3rem] lg:text-[3rem]'>Welcome !</h1>
+          <img src={Welcome_Logo} alt="Welcome Logo" className='max-sm:w-[20.04%] max-sm:h-[18.14%] rounded-md max-md:w-[17%] max-md:h-[9%] m-6 max-lg:w-[15%] max-lg:mt-0 lg:w-[15%] lg:mt-0' />
           
-          <Button_WP label="Get Started" className='max-sm:w-[80%]' />
+          <Button_WP label="Get Started" className='max-sm:w-[80%] max-md:w-[90%] max-md:text-2xl max-md:p-4 max-lg:w-[80%] max-lg:p-4 max-lg:text-3xl lg:w-[80%] lg:p-4 lg:text-3xl' />
         
-          <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" className='max-sm:w-[100%] max-sm:h-[60px] svg-animate'><g fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2}><path d="M12 3l0 17.5" /><path d="M12 21l7 -7M12 21l-7 -7" /></g>
+      <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" className='max-sm:w-[100%] max-sm:h-[60px] svg-animate max-md:w-[50%] max-md:h-[70px] max-lg:w-[30%] max-lg:h-[50px] lg:w-[30%] lg:h-[50px]'><g fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2}><path d="M12 3l0 17.5" /><path d="M12 21l7 -7M12 21l-7 -7" /></g>
           </svg>
 
       {loading ?  (<h2 className="text-2xl fade-in-scale">Getting Started .....</h2>) :
             <>  
-            <InputField placeholder='What Should We Call You ?' required setUserProfileResults={setUserProfileResults} /> 
+            <InputField placeholder='What Should We Call You ?' required setUserProfileResults={setUserProfileResults} className='max-md:w-[80%] p-4 max-md:placeholder:text-2xl max-lg:w-[80%] max-md:text-2xl max-lg:text-3xl'/> 
           
-          <Button_WP label="Start" className='max-sm:w-[50%]' onClick={handleGetStarted} disabled={loading} />
+          <Button_WP label="Start" className='max-sm:w-[50%] max-md:w-[50%] max-md:text-4xl max-md:mt-10 mb-10 max-lg:w-[60%] max-lg:text-3xl max-lg:mt-10 lg:w-[60%]' onClick={handleGetStarted} disabled={loading} />
           </>
          }
         
